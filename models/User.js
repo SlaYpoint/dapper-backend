8aaf35160a8b10c1cca1561eb6e5fc1c54e189f6
SlaYpoint <codersaysady@gmail.com>
2021-07-31 13:25:04 +0530

    added auth methods

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
 
 const UserSchema = mongoose.Schema({
   name: {
@@ -31,6 +32,19 @@ UserSchema.pre('save', async function (next) {
   
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
+
+// Match user given password with hashed password
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+}
+
+// Sign a JWT amd return
+UserSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE
+  });
+}
 
 module.exports = mongoose.model('User', UserSchema);
